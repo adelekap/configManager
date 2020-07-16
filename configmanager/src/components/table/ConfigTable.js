078193e32a887f084d72b0b1bc861eafafce0bc1
adelekap <adelejkap@gmail.com>
2020-07-15 18:22:36 -0700

    functioning editable table

--- configmanager/src/components/table/ConfigTable.js
+++ configmanager/src/components/table/ConfigTable.js
@@ -2,12 +2,11 @@ import React, { useState, useRef, useEffect, useContext } from "react";
 import { Table } from "antd";
 
 import { columns } from "./columns";
-import { TableContext } from "./TableContext"
+import { TableContext } from "./TableContext";
 
-
-const ConfigCell = ({children, editable, dataIndex, record}) => {
+const ConfigCell = ({ children, editable, dataIndex, record }) => {
   const [isEditting, setEditting] = useState(false);
-  const {updateCell} = useContext(TableContext)
+  const { updateCell } = useContext(TableContext);
   const inputRef = useRef();
 
   // This function will run whenever isEdding changes value
@@ -15,17 +14,29 @@ const ConfigCell = ({children, editable, dataIndex, record}) => {
     if (isEditting) inputRef.current.focus();
   }, [isEditting]);
 
-  const changeCellValue = ({key, currentTarget:{value}}) => {
-    if(key === 'Enter'){
-        setEditting(false)
-        updateCell(dataIndex, record.dataIndex, value)
+  const changeCellValue = () => {
+    if (isEditting) {
+      const cellValue = inputRef.current.value || record.value
+      updateCell(dataIndex, record.dataIndex, cellValue);
+      setEditting(false);
+    } else {
+      setEditting(!isEditting && editable);
     }
-}
+  };
 
-  const edittingForm = <input ref={inputRef} onKeyUp={changeCellValue} type="text" placeholder={record.value}></input>
+  const edittingForm = (
+    <input
+      ref={inputRef}
+      onKeyUp={({ key }) => {
+        if (key === "Enter") changeCellValue();
+      }}
+      type="text"
+      placeholder={record.value}
+    ></input>
+  );
   const normalView = <span>{children}</span>;
   const toggleableView = (
-    <div onClick={() => setEditting(!isEditting && editable)}>
+    <div onClick={changeCellValue}>
       {isEditting ? edittingForm : normalView}
     </div>
   );
@@ -36,24 +47,24 @@ const ConfigCell = ({children, editable, dataIndex, record}) => {
 export const ConfigTable = () => {
   // Add a unique value field to each piece of data, in this case, its index in the array
 
-  const {tableState} = useContext(TableContext)
+  const { tableState } = useContext(TableContext);
 
-
-  const tableColumns = columns.map(col => {
+  const tableColumns = columns.map((col) => {
     return {
       ...col,
-      onCell: record => ({
+      onCell: (record) => ({
         record,
         editable: col.editable,
         dataIndex: col.dataIndex,
         title: col.title,
       }),
-    };})
+    };
+  });
 
   return (
     <Table
-      components={{ body: { cell: ConfigCell} }}
-      dataSource= {tableState}
+      components={{ body: { cell: ConfigCell } }}
+      dataSource={tableState}
       columns={tableColumns}
       rowKey="dataIndex"
     />
